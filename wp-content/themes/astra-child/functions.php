122f8034c7444e68583d6d5cf72051a686e91dda
pranaySpark18 <evincedevapi@gmail.com>
2024-12-11 16:59:14 +0530

    created register form

--- wp-content/themes/astra-child/functions.php
+++ wp-content/themes/astra-child/functions.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Astra Child Theme functions and definitions
  *
@@ -11,36 +12,48 @@
 /**
  * Define Constants
  */
-define( 'CHILD_THEME_ASTRA_CHILD_VERSION', '1.0.1' );
+define('CHILD_THEME_ASTRA_CHILD_VERSION', '1.0.1');
 
 //required files
-require_once get_stylesheet_directory().'/shortcodes/util_shortcodes.php';
-require_once get_stylesheet_directory().'/shortcodes/jobs_cat_carousel.php';
+require_once get_stylesheet_directory() . '/shortcodes/util_shortcodes.php';
+require_once get_stylesheet_directory() . '/shortcodes/jobs_cat_carousel.php';
+require_once get_stylesheet_directory() . '/shortcodes/register_form.php';
 
 
 
 /**
  * Enqueue styles
  */
-function child_enqueue_styles() {
-	wp_enqueue_style( 'astra-child-theme-css', get_stylesheet_directory_uri() . '/style.css', array('astra-theme-css'), CHILD_THEME_ASTRA_CHILD_VERSION, 'all' );
-	wp_enqueue_style( 'astra-child-theme-shortcode-css', get_stylesheet_directory_uri() . '/shortcodes/shortcode_styles.css', array('astra-theme-css'), CHILD_THEME_ASTRA_CHILD_VERSION, 'all' );
-	wp_enqueue_script( 'font-awesome', 'https://kit.fontawesome.com/46ce832a93.js', array(), null, false );
-	wp_enqueue_style( 'slick-css',  get_stylesheet_directory_uri()."/assets/slick/slick.css");
-	wp_enqueue_style( 'slick-theme-css',  get_stylesheet_directory_uri()."/assets/slick/slick-theme.css");
+function child_enqueue_styles()
+{
+	wp_enqueue_style('astra-child-theme-css', get_stylesheet_directory_uri() . '/style.css', array('astra-theme-css'), CHILD_THEME_ASTRA_CHILD_VERSION, 'all');
+	wp_enqueue_style('astra-child-theme-shortcode-css', get_stylesheet_directory_uri() . '/shortcodes/shortcode_styles.css', array('astra-theme-css'), CHILD_THEME_ASTRA_CHILD_VERSION, 'all');
+	wp_enqueue_script('font-awesome', 'https://kit.fontawesome.com/46ce832a93.js', array(), null, false);
+	wp_enqueue_style('slick-css',  get_stylesheet_directory_uri() . "/assets/slick/slick.css");
+	wp_enqueue_style('slick-theme-css',  get_stylesheet_directory_uri() . "/assets/slick/slick-theme.css");
 }
 
-add_action( 'wp_enqueue_scripts', 'child_enqueue_styles', 15 );
+add_action('wp_enqueue_scripts', 'child_enqueue_styles', 15);
 
 
-if ( ! function_exists( 'header_scripts' ) ) {
-	function header_scripts() {
-		?>
-<script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
-  <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
-  <script type="text/javascript" src=<?php echo get_stylesheet_directory_uri()."/assets/slick/slick.min.js" ?>></script>
-
+if (! function_exists('header_scripts')) {
+	function header_scripts()
+	{
+?>
+		<script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+		<script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+		<script type="text/javascript" src=<?php echo get_stylesheet_directory_uri() . "/assets/slick/slick.min.js" ?>></script>
 <?php
 	}
 }
-add_action( 'wp_head', 'header_scripts' );
+add_action('wp_head', 'header_scripts');
+
+// Separate Page for register
+add_action('template_redirect', 'redirect_logged_in_users_to_my_account');
+function redirect_logged_in_users_to_my_account()
+{
+	if (is_page('register') && is_user_logged_in()) {
+		wp_redirect(get_permalink(get_option('woocommerce_myaccount_page_id')));
+		exit;
+	}
+}
--- wp-content/themes/astra-child/shortcodes/util_shortcodes.php
+++ wp-content/themes/astra-child/shortcodes/util_shortcodes.php
@@ -1,77 +1,80 @@
 <?php
 //login button
-function login_cb(){
-	ob_start();
-	?>
-	<button class="login-button-header" style="padding:15px">
-	<?php  
-	if(is_user_logged_in()){
-		echo '<a href="/referrify/my-account/" style="color:white;">Account</a>';
-	}else{
-		echo '<a href="/referrify/my-account/" style="color:white;">Sign In</a>';
-	}
-	?>
-	</button>
-	<?php
+function login_cb()
+{
+    ob_start();
+?>
+    <button class="login-button-header" style="padding:15px">
+        <?php
+        if (is_user_logged_in()) {
+            echo '<a href="/referrify/my-account/" style="color:white;">Account</a>';
+        } else {
+            echo '<a href="/referrify/my-account/" style="color:white;">Sign In</a>';
+        }
+        ?>
+    </button>
+<?php
     return ob_get_clean();
 }
 
-add_shortcode('login_button','login_cb');
+add_shortcode('login_button', 'login_cb');
 
 //banner category button / tags
 
-function banner_category_buttons_cb(){
-	ob_start();
+function banner_category_buttons_cb()
+{
+    ob_start();
     $categories = get_terms(
         array(
             'taxonomy'   => 'product_cat',
             'orderby'    => 'name',
             'hide_empty' => false,
-            'number'     => 5, 
+            'number'     => 5,
         )
     );
-    ?><div class="job-category-nav-container"><?php
-     if ( ! empty( $categories ) && ! is_wp_error( $categories ) ) {
-        foreach ( $categories as $category ) {
-            
-        ?>
-          <a href="<?php echo esc_url( get_term_link( $category ) ); ?>" class="quick-category-banner"><?php echo esc_html( $category->name ); ?></a>
+?><div class="job-category-nav-container"><?php
+                                                if (! empty($categories) && ! is_wp_error($categories)) {
+                                                    foreach ($categories as $category) {
+
+                                                ?>
+                <a href="<?php echo esc_url(get_term_link($category)); ?>" class="quick-category-banner"><?php echo esc_html($category->name); ?></a>
         <?php
-        }
-     }?>
-     </div>
-     <?php
+                                                    }
+                                                } ?>
+    </div>
+<?php
     return ob_get_clean();
 }
 
-add_shortcode('banner_category_buttons','banner_category_buttons_cb');
+add_shortcode('banner_category_buttons', 'banner_category_buttons_cb');
 
-function search_job(){
+function search_job()
+{
     ob_start();
-    ?>
+?>
     <div class='search-job-container'>
-    <form role="search" method="get" class="search-job" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-    <!-- Jon Search Field -->
-     <div class="search-job-dv">
-    <input type="search" class="search-job-field" 
-           placeholder="<?php echo esc_attr__( 'Enter skills', 'woocommerce' ); ?>" 
-           value="<?php echo get_search_query(); ?>" name="s" />
+        <form role="search" method="get" class="search-job" action="<?php echo esc_url(home_url('/')); ?>">
+            <!-- Jon Search Field -->
+            <div class="search-job-dv">
+                <input type="search" class="search-job-field"
+                    placeholder="<?php echo esc_attr__('Enter skills', 'woocommerce'); ?>"
+                    value="<?php echo get_search_query(); ?>" name="s" />
+            </div>
+            <!-- Location Field -->
+            <div>
+                <input type="text" class="location-field"
+                    placeholder="<?php echo esc_attr__('Location', 'woocommerce'); ?>"
+                    value="<?php echo isset($_GET['location']) ? esc_attr($_GET['location']) : ''; ?>" name="location" />
+            </div>
+            <!-- Submit Button -->
+            <button type="submit" class="search-submit"><span><?php echo esc_html__('Search', 'woocommerce'); ?></span></button>
+
+            <!-- Hidden job Type -->
+            <input type="hidden" name="post_type" value="product" />
+        </form>
     </div>
-    <!-- Location Field -->
-    <div>
-    <input type="text" class="location-field" 
-           placeholder="<?php echo esc_attr__( 'Location', 'woocommerce' ); ?>" 
-           value="<?php echo isset( $_GET['location'] ) ? esc_attr( $_GET['location'] ) : ''; ?>" name="location" />
-    </div>
-    <!-- Submit Button -->
-    <button type="submit" class="search-submit"><span><?php echo esc_html__( 'Search', 'woocommerce' ); ?></span></button>
-    
-    <!-- Hidden job Type -->
-    <input type="hidden" name="post_type" value="product" />
-</form>
-</div>
-    <?php
+<?php
     return ob_get_clean();
 }
 
-add_shortcode('search_job','search_job');
+add_shortcode('search_job', 'search_job');
